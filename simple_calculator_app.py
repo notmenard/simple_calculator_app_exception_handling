8f5356d2f040264bed69f5bf07fcaa5c734df17c
Menard <menardsabugo@gmail.com>
2024-05-09 00:12:23 +0800

    Assigning the operations to be performed

--- simple_calculator_app.py
+++ simple_calculator_app.py
@@ -2,4 +2,4 @@
 # Displaying the list of operations on the calculator
 # Prompts the user to enter their choice of operation
 # Prompts for the user to enter two numbers
-
+# Perform the user's selected operation and displaying their result
