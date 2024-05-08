dc6dd2ba8b4457651799a414ea7652e60b2644ea
Menard <menardsabugo@gmail.com>
2024-05-09 00:17:15 +0800

    Assigning a while loop

--- simple_calculator_app.py
+++ simple_calculator_app.py
@@ -1,4 +1,6 @@
 # Displaying the introduction for the simple calculator application
+while True:
+
 # Displaying the list of operations on the calculator
 # Prompts the user to enter their choice of operation
 # Prompts for the user to enter two numbers
