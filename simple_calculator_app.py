2e4bea1c54c1d2d127386f1734894d8c0635e708
Menard <menardsabugo@gmail.com>
2024-05-09 00:11:02 +0800

    Prompts for the user to enter two numbers

--- simple_calculator_app.py
+++ simple_calculator_app.py
@@ -1,3 +1,5 @@
 # Displaying the introduction for the simple calculator application
 # Displaying the list of operations on the calculator
 # Prompts the user to enter their choice of operation
+# Prompts for the user to enter two numbers
+
