f640b8134879ef22588be0e4cd2d0e47fd9f8195
Menard <menardsabugo@gmail.com>
2024-05-10 18:47:00 +0800

    Added Revisions

--- simple_calculator_app.py
+++ simple_calculator_app.py
@@ -20,7 +20,7 @@ while True:
             break
         except Exception as e:
             print("\033[91m" + str(e) + "\033[0m")
-            print("\033[91m" + "An unexpected error occurred. Please try the application again." + "\033[0m")
+            print("\033[91m" + "An unexpected error occurred. Please choose a valid option." + "\033[0m")
 
     # Prompts for the user to enter two numbers
     while True:
@@ -36,28 +36,32 @@ while True:
             if choice == "1":
                 result = (first_number + second_number)
                 print("\033[95m" + f"Result: {result}" + "\033[0m")
+                print()
             elif choice == "2":
                 result = (first_number - second_number)
                 print("\033[95m" + f"Result: {result}" + "\033[0m")
+                print()
             elif choice == "3":
                 result = (first_number * second_number)
                 print("\033[95m" + f"Result: {result}" + "\033[0m")
+                print()
             elif choice == "4":
                 try:
                     result = (first_number // second_number)
                     print("\033[95m" + f"Result: {result}" + "\033[0m")
+                    print()
                 except ZeroDivisionError as e:
                     print("\033[91m" + str(e) + "\033[0m")
                     print("\033[91m" + "Invalid input. Please enter a valid number." + "\033[0m")
+                    print()
             break
 
     # Adding a prompt for the user asking if they want to try again
-    print()
     try_again = input("\033[92m" + "Do you want to try again? ('y' for yes / 'n' for no): " + "\033[0m")
-    if try_again.lower() not in ["y", "n"]:
-        raise ValueError("Invalid choice. Please choose a valid option.")
-    elif try_again.lower() != 'y':
-        # Display a thank you message and exit the program
+    if try_again.lower() == 'y':
+        continue
+    elif try_again.lower() == 'n':
         print("\033[94m" + " Thank you for using the Simple Calculator App! ".center(73, "-") + "\033[0m")
         break
-    
+    else:
+        print("\033[91m" + "Invalid choice. Please choose a valid option." + "\033[0m")
